b0c2647d2bdf6a1d37b851d83f4dba8d07624048
Mohamed Emam <mohamed.emam93@outlook.fr>
2020-12-19 16:47:45 +0100

    Ajout du modèle Documents

---
+++ src/main/java/com/episen/ing3/fise/springbootlockauthentification/model/Documents.java
@@ -0,0 +1,34 @@
+package com.episen.ing3.fise.springbootlockauthentification.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+@Data
+@Slf4j
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class Documents {
+
+    @Id
+    String documentId;
+
+    @CreatedDate
+    private LocalDateTime created;
+    @LastModifiedDate
+    private LocalDateTime updated;
+
+    String title ;
+    String creator;
+    String editor;
+    String body ;
+}
