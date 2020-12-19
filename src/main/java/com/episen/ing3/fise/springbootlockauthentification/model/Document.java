f21aa2eab0277d9702fb31a43b4dcce34e821532
Mohamed Emam <mohamed.emam93@outlook.fr>
2020-12-19 16:10:41 +0100

    Ajout package modele

---
+++ src/main/java/com/episen/ing3/fise/springbootlockauthentification/model/Document.java
@@ -0,0 +1,18 @@
+package com.episen.ing3.fise.springbootlockauthentification.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Document {
+
+    String documentId;
+
+}
