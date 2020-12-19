24b5db9d85616ccfffc51d61d8a660341484dcd5
lucas.meunier <lucas.meunier@etu.u-pec.fr>
2020-12-19 13:14:50 +0100

    Initial commit
    Add an empty springboot project

---
+++ src/main/java/com/episen/ing3/fise/springbootlockauthentification/SpringbootLockAuthentificationApplication.java
@@ -0,0 +1,13 @@
+package com.episen.ing3.fise.springbootlockauthentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLockAuthentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLockAuthentificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/episen/ing3/fise/springbootlockauthentification/SpringbootLockAuthentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.episen.ing3.fise.springbootlockauthentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLockAuthentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
